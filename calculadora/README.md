313b0032c51e2bfba0c1bdf45ad4a11368e7f7c6
Lucas Braich <lucasferreira.5890@aluno.unibh.br>
2023-05-16 18:41:24 -0300

    Commit do trabalho

---
+++ calculadora/README.md
@@ -0,0 +1,11 @@
+# Calculadora
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Lucas-Braich-Ferreira/pen/JjmaBgJ](https://codepen.io/Lucas-Braich-Ferreira/pen/JjmaBgJ).
+
+A calculadora deve aceitar as 4 operações básicas da matemática.
+
+Deverá ser possível incluir vários valores com diferentes operações em uma única conta,
+por exemplo: 2+4*5-10.
+
+A memória de valores e suas operações deve ser mostrada em rotulo dinâmico acima do
+objeto de input de valores.
---
+++ calculadora/dist/script.js
@@ -0,0 +1,30 @@
+let display = document.getElementById("display");
+let memory = document.getElementById("memory");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearEntry() {
+  display.value = "";
+}
+
+function clearDisplay() {
+  display.value = "";
+  memory.innerHTML = "";
+}
+
+function calculate() {
+  let expression = display.value;
+  let result;
+
+  try {
+    result = eval(expression);
+    memory.innerHTML += expression + " = " + result + "<br>";
+  } catch (error) {
+    result = "Erro";
+    memory.innerHTML += expression + " = " + result + "<br>";
+  }
+
+  display.value = result;
+}
